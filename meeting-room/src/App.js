091db9331cf85446d35050f451941e3ec2af992f
uchejustin <uchejustin@yahoo.com>
2020-08-01 19:09:28 +0300

    added boiler plate index.js and app.js files

---
+++ meeting-room/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Our First React App</h1>
+        </header>
+        <div>
+          <p>Hello World</p>
+          <p>I added some text here</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
